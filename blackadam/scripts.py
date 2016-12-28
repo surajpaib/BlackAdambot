d2854389cc6aeae320b2f40b0a2be751436a9203
Suraj Pai <surajballambat@gmail.com>
2016-12-28 19:30:29 +0530

    Heroku Deploy

---
+++ blackadam/scripts.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+'''
+    >>> python test.py test.mp3
+'''
+from acrcloud.recognizer import ACRCloudRecognizer
+
+def audio_file(audio):
+
+    config = {
+        'host': 'ap-southeast-1.api.acrcloud.com',
+        'access_key': 'ea240a5363df44473faac1d47ef35448',
+        'access_secret': 'unp9CHJZYRkVkzljCPqf6BchfsLMTFo7xeEiz113',
+        'debug': True,
+        'timeout': 30  # seconds
+    }
+
+    '''This module can recognize ACRCloud by most of audio/video file.
+        Audio: mp3, wav, m4a, flac, aac, amr, ape, ogg ...
+        Video: mp4, mkv, wmv, flv, ts, avi ...'''
+    re = ACRCloudRecognizer(config)
+
+    # recognize by file path, and skip 0 seconds from from the beginning of sys.argv[1].
+    print(re.recognize_by_file(audio, 1))
+
+    print("duration_ms=" + str(ACRCloudRecognizer.get_duration_ms_by_file(audio)))
+
+    buf = open(audio, 'rb').read()
+    # recognize by file_audio_buffer that read from file path, and skip 0 seconds from from the beginning of sys.argv[1].
+    print re.recognize_by_filebuffer(buf, 1)
+    return re.recognize_by_filebuffer(buf,1)
+    # aa.wav must be (RIFF (little-endian) data, WAVE audio, Microsoft PCM, 16 bit, mono 8000 Hz)
+    # buf = open('aa.wav', 'rb').read()
+    # buft = buf[1024000:192000+1024001]
+    # recognize by audio_buffer(RIFF (little-endian) data, WAVE audio, Microsoft PCM, 16 bit, mono 8000 Hz)
+    # print re.recognize(buft)
---
+++ blackadam/views.py
@@ -0,0 +1,98 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
+import wget
+import requests
+import os
+
+
+# Create your views here.
+from blackadam.scripts import audio_file
+
+
+
+PAGE_TOKEN='EAATCCukVUwkBAI9ZBY5kHy12ZCIMaogxuLxNnhgEnfUkpZB9mxTdxWmBnhsZCuE50UYFLoHJNStNTJAbcw64k8KUWFrT9xJtgLuyRbE35PZBh7uY6or8VZAIoJ4R6Jw3KUAQFwIJNFSLRiqLOEiKatHetVstvlpi8OH4a8vi2AoAZDZD'
+
+
+
+@csrf_exempt
+def webhook(request):
+    if (request.method=="GET"):
+        if(request.GET['hub.verify_token']=="blackadam"):
+            return HttpResponse(request.GET['hub.challenge'])
+        else:
+            return HttpResponse("Wrong verification token")
+
+    if (request.method=="POST"):
+        dict=json.loads(request.body.decode('utf-8'))
+
+        try:
+            for entry in dict['entry']:
+
+                for message in entry['messaging']:
+
+                    recipient_id = message['sender']['id']
+
+
+
+                    '''
+                    Get Started Button Code
+
+                    '''
+                    try:
+                        if message["postback"]["payload"]=="START":
+                            welcome_message="Welcome! I am Black Adam, your personal Music Butler! To record your music click on the mic button and I'll tell you whatever I know!"
+                            post_message(recipient_id,welcome_message)
+                            return HttpResponse(status=200)
+                    except:
+                        print "Continue"
+
+
+                    try:
+                        if message["message"]["text"]!=None:
+                            err_message = "I can help you out with identifying music for now. Click the record button to witness me!"
+                            post_message(recipient_id,err_message)
+                            return HttpResponse(status=200)
+                            break
+                            break
+
+                    except:
+                        for attachment in message["message"]["attachments"]:
+                            audio=attachment["payload"]["url"]
+                            print audio
+                            wget.download(audio,out="music.mp3")
+                            return_object=audio_file("music.mp3")
+                            os.remove("music.mp3")
+                            return_object=json.loads(return_object.decode('utf-8'))
+
+                            for t in return_object["metadata"]["music"]:
+                                song=t["title"]
+                                song="The title of the song is,  \""+song+" \""
+                                post_message(recipient_id,song)
+                                post_message(recipient_id,"The artists are,")
+                                for a in t["artists"]:
+                                    artist=a["name"]
+                                    post_message(recipient_id,artist)
+                                post_message(recipient_id,"All done here, record another clip?")
+
+                                break
+
+
+                            return HttpResponse(return_object,status=200,content_type="application/json")
+
+
+
+
+        except:
+            return HttpResponse(status=200)
+
+
+
+
+
+def post_message(recipient_id,message):
+    post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token='+PAGE_TOKEN
+    response_msg = json.dumps({"recipient": {"id": recipient_id}, "message": {"text": message}})
+    status = requests.post(post_message_url, headers={"Content-Type": "application/json"}, data=response_msg)
+    print(status.json())
