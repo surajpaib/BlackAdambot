7b0021d77437a61e8b2a517d127861061dc4394a
Suraj Pai <surajballambat@gmail.com>
2016-12-29 20:01:52 +0530

    db_config updated

--- BlackAdambot/settings.py
+++ BlackAdambot/settings.py
@@ -74,7 +74,7 @@ WSGI_APPLICATION = 'BlackAdambot.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
-db_from_env=dj_database_url.config(conn_max_Age=500)
+db_from_env=dj_database_url.config(conn_max_age=500)
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
